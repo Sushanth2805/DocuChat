ddf63d9bdc674e3b3498eb327737d7b305f7dea7
Sushanth2805 <sushanth.2805@gmail.com>
2024-06-24 05:47:10 +0530

    Update docuchat.py

--- docuchat.py
+++ docuchat.py
@@ -1,19 +1,17 @@
 import streamlit as st
 import pdfplumber
 import os
-from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 import google.generativeai as genai
 from langchain.vectorstores import FAISS
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
-from dotenv import load_dotenv
 import io
 
-load_dotenv()
-
-genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+# Configure the API key
+genai.configure(api_key=st.secrets["GOOGLE_API_KEY"])
 
 def get_pdf_text(pdf_docs):
     text = ""
